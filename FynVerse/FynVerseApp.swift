e06083689006ef804e31b8c413e47c49bf38cb6d
Zubair <zubairahmed20050831@gmail.com>
2025-07-13 15:10:23 +0530

    Initial Commit

---
+++ FynVerse/FynVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FynVerseApp.swift
+//  FynVerse
+//
+//  Created by zubair ahmed on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FynVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
